2c46aa312ee1754df165a8e69929ab3859ae414f
Diego-EM <dmunozaguilar2000@gmail.com>
2022-04-10 16:21:43 -0500

    port variable changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dateController = require('./controller/dateController')
 const app = express();
 
-app.set('port',process.env.PORT | 3000);
+app.set('port',process.env.PORT || 3000);
 
 //Routes
 app.get('/api/:date',(req,res)=>{
